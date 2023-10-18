4785491f6cd156641312de5608c79921ab316750
in2dm3 <nadir.elhasnaoui@elorrieta-errekamari.com>
2023-10-18 08:26:45 +0200

    Agregar archivos de proyecto.

---
+++ Config.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherApp
+{
+    public class Config
+    {
+        //pongo aqui mi class geoapify
+        public static string GeoApifykey = "183439f2e4fe46e3a5fc467417be6f21";
+    
+    }
+}
---
+++ Endpoint.cs
@@ -0,0 +1,18 @@
+﻿using WeatherApp;
+
+namespace WheatherApp
+{
+    public class Endpoint
+    {
+        public static string getGeoApify(string city) {
+         
+            return $"https://api.geoapify.com/v1/geocode/search?text={city}&format=json&apiKey={Config.GeoApifykey}"; 
+            
+        }
+        public static string GetWeatherEndPoint(float lat, float lon)
+        {
+            var url = $"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m,relativehumidity_2m&hourly=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=Europe%2FLondon";
+            return url;
+        }
+    }
+}
